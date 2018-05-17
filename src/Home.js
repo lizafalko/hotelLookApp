9800edb325e2cdba44d9a4315e02bcec89b0821e
lizafalko <liza199797@mail.ru>
2018-05-17 14:30:27 +0300

    added new blocks

--- src/Home.js
+++ src/Home.js
@@ -2,10 +2,45 @@ import React, { Component } from 'react';
 import ReactDOM from 'react';
 import './App.css';
 
-export const Home = () => (
-  <div className="page">
-    <h2 className="page__heading">Welcome guys!</h2>
-  </div>
-);
+export class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.handleClose = this.handleClose.bind(this);
+  }
+
+  handleClose() {
+
+  }
+
+  render() {
+    return (
+      <div className="page page__page--modification">
+        <h2 className="page__heading">Welcome guys!</h2>
+        <div className="page__close-icon">
+          <h3>How does it work?</h3>
+          <i className="times-icon fa fa-times close_icon" onClick={this.handleClose} />
+        </div>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+          incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+          exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+          Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+          incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+          exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+          Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+          incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+          exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+          Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+        <h4>Creation date: {(new Date()).toDateString()}</h4>
+      </div>
+    )
+  }
+}
 
 export default Home;
