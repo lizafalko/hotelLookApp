8f30cc93b78792c1571fefaeeee93ef8f1bf8cde
lizafalko <lizafloyd2@gmail.com>
2020-07-05 19:46:01 +0300

    added new banner styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
-// import logo from './logo.svg';
 import Advantages from './Advantages';
 import Info from './Info';
+import Banner from './Banner';
+import About from './About';
 
 function App() {
   return (
     <div className="app">
+      <Banner />
+      <About />
       <Advantages />
       <Info />
     </div>
