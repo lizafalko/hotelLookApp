e224b5146c6ad6661ca2278ba44aa48b22de8cd5
lizafalko <lizafloyd2@gmail.com>
2020-07-05 16:09:34 +0300

    added new deployment

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
+import './fonts/DinWebNarrowDemi.ttf'
+import './fonts/DINPro-CondBold.otf'
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
