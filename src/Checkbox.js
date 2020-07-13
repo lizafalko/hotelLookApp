0cec0c944e3f98be23332ad4fe6d11f393c07bad
lizafalko <lizafloyd2@gmail.com>
2020-07-13 13:34:54 +0300

    added new required blocks

--- src/Checkbox.js
+++ src/Checkbox.js
@@ -4,7 +4,7 @@ import Linkify from 'react-linkify';
 export function Checkbox(props) {
   return (
     <label className={props.checkboxClass}>
-      <input className="checkbox__input" name="checkbox" type="checkbox" />
+      <input className="checkbox__input" name="checkbox" type="checkbox" required/>
       <span className="checkbox__checkmark"></span>
       <span className="checkbox__text">
         <Linkify>Я согласен с условиями обработки <a className="form__link" style={{color: '#3D3BFF'}} rel="noopener noreferrer" target="_blank" href="https://skillbox.ru/privacy_policy.pdf">персональных данных</a></Linkify>
--- src/Form.js
+++ src/Form.js
@@ -8,9 +8,9 @@ export function Form(props) {
         <span className="form__text form__text_grey">Старт курса: <span className="form__text form__text_blue">{props.courseDate}</span></span>
         <span className="form__text form__text_grey">Осталось: <span className="form__text form__text_blue">{props.coursePlaces}</span></span>
       </div>
-      <input className="form__field form__field_input" type="text" name="name" placeholder="Имя" />
-      <input className="form__field form__field_input" type="text" name="surname" placeholder="Телефон" />
-      <input className="form__field form__field_input" type="text" name="mail" placeholder="Email" />
+      <input className="form__field form__field_input" type="text" name="name" placeholder="Имя" required />
+      <input className="form__field form__field_input" type="tel" name="phone" placeholder="Телефон" required />
+      <input className="form__field form__field_input" type="email" name="mail" placeholder="Email" required />
       <Checkbox checkboxClass="checkbox checkbox_mobile" />
       <button className="form__field form__field_button" type="submit">Записаться</button>
       <Checkbox checkboxClass="checkbox checkbox_desktop" />
